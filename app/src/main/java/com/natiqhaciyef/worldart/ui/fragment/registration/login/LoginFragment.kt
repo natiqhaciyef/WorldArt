31bea66bd5f326d3cc3d128f7db9baa96d3df348
natighajiyev <natig.hajiyev1@gmail.com>
2023-12-28 10:53:31 -0800

    Login UI created and bugs fixed

--- app/src/main/java/com/natiqhaciyef/worldart/ui/fragment/registration/login/LoginFragment.kt
+++ app/src/main/java/com/natiqhaciyef/worldart/ui/fragment/registration/login/LoginFragment.kt
@@ -3,6 +3,8 @@ package com.natiqhaciyef.worldart.ui.fragment.registration.login
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +18,7 @@ import java.net.URLEncoder
 @AndroidEntryPoint
 class LoginFragment : BaseFragment() {
     private lateinit var binding: FragmentLoginBinding
+    private var toggle = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +30,10 @@ class LoginFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
 
+    private fun setup() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             binding.goToSignUp.text =
                 Html.fromHtml(
@@ -36,6 +42,21 @@ class LoginFragment : BaseFragment() {
                 )
         } else {
             binding.goToSignUp.text = Html.fromHtml(getString(R.string.you_don_t_have_an_account))
+        }
+
+
+        binding.visibilityIcon.setOnClickListener {
+            if (toggle) {
+                binding.passwordTextInputLogin.transformationMethod =
+                    PasswordTransformationMethod.getInstance()
+                binding.visibilityIcon.setImageResource(R.drawable.visibility_off_icon)
+            } else {
+                binding.passwordTextInputLogin.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+                binding.visibilityIcon.setImageResource(R.drawable.visibility_icon)
+            }
+
+            toggle = !toggle
         }
     }
 }
