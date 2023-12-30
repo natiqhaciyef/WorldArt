136bc02015fa2e01f4810465ad326575b5341b08
natighajiyev <natig.hajiyev1@gmail.com>
2023-12-30 09:09:36 -0800

    Home post architecture and UI widget created. Post remote architecture implemented

--- app/src/main/java/com/natiqhaciyef/worldart/domain/di/AppModule.kt
+++ app/src/main/java/com/natiqhaciyef/worldart/domain/di/AppModule.kt
@@ -4,18 +4,21 @@ import com.natiqhaciyef.worldart.data.source.ArchitectureSource
 import com.natiqhaciyef.worldart.data.source.FirebaseSource
 import com.natiqhaciyef.worldart.data.source.HistorySource
 import com.natiqhaciyef.worldart.data.source.PaintingSource
+import com.natiqhaciyef.worldart.data.source.PostSource
 import com.natiqhaciyef.worldart.data.source.ScienceSource
 import com.natiqhaciyef.worldart.data.source.TravelSource
 import com.natiqhaciyef.worldart.domain.repository.ArchitectureRepository
 import com.natiqhaciyef.worldart.domain.repository.FirebaseRepository
 import com.natiqhaciyef.worldart.domain.repository.HistoryRepository
 import com.natiqhaciyef.worldart.domain.repository.PaintingRepository
+import com.natiqhaciyef.worldart.domain.repository.PostRepository
 import com.natiqhaciyef.worldart.domain.repository.ScienceRepository
 import com.natiqhaciyef.worldart.domain.repository.TravelRepository
 import com.natiqhaciyef.worldart.domain.repository.impl.ArchitectureRepositoryImpl
 import com.natiqhaciyef.worldart.domain.repository.impl.FirebaseRepositoryImpl
 import com.natiqhaciyef.worldart.domain.repository.impl.HistoryRepositoryImpl
 import com.natiqhaciyef.worldart.domain.repository.impl.PaintingRepositoryImpl
+import com.natiqhaciyef.worldart.domain.repository.impl.PostRepositoryImpl
 import com.natiqhaciyef.worldart.domain.repository.impl.ScienceRepositoryImpl
 import com.natiqhaciyef.worldart.domain.repository.impl.TravelRepositoryImpl
 import dagger.Module
@@ -53,6 +56,11 @@ object AppModule {
     @Singleton
     fun provideTravelRepository(ds: TravelSource) =
         TravelRepositoryImpl(ds) as TravelRepository
+
+    @Provides
+    @Singleton
+    fun providePostRepository(ds: PostSource) =
+        PostRepositoryImpl(ds) as PostRepository
 
 
     // firebase
